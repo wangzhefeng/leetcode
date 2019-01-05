7aca0b7322322aafb66069da6826db577f81260b
Wangzf <wangzhefengr@163.com>
2019-01-06 01:33:01 +0800

    start project

---
+++ p001_twoSum.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+@author: tinker
+@date: 2019-01-06
+"""
+class Solution:
+
+	def __init__(self, nums, target):
+		self.nums = nums
+		self.target = target
+
+	def twoSum(self, nums, target):
+		"""
+		:type nums: list[int]
+		:type target: int
+		:return type: list[int]
+		:param nums:
+		:param target:
+		:return:
+		"""
+		dic = {}
+		for i, num in enumerate(nums):
+			if num in dic:
+				return [dic[num], i]
+			else:
+				dic[target - num] = i
+
+
---
+++ playground.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+@author:
+@date:
+"""
+
+
+import p001_twoSum
+
+
+def main():
+	Solution.twoSum(nums = [2, 7, 11, 15], target = 9)
+
+if __name__ == "__main__":
+	main()
+
+
+
